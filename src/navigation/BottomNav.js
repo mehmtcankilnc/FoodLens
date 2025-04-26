10942a53f1fbbed2f4e29a2dd1c8b2ba6f89e646
Fatma Nur Iskal <fnuriskal@gmail.com>
2025-04-26 17:56:38 +0300

    homePage

--- src/navigation/BottomNav.js
+++ src/navigation/BottomNav.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/main/HomeScreen";
 import ScanScreen from "../screens/main/ScanScreen";
 import Profile from "../screens/main/Profile";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import { widthPercentageToDP as wp } from "react-native-responsive-screen";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,27 +14,87 @@ export default function BottomNav() {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: "#52c46f",
+        tabBarInactiveTintColor: "black",
+        tabBarStyle: {
+          position: "absolute",
+          bottom: wp("5%"),
+          height: wp("15%"),
+          marginHorizontal: wp("4%"),
+          backgroundColor: "#f8f8f8",
+          borderRadius: 20,
+          elevation: 5,
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 5,
+          },
+          shadowOpacity: 0.1,
+          shadowRadius: 5,
+        },
       }}
     >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          tabBarIcon: () => <AntDesign name="home" size={24} color="black" />,
+          tabBarIcon: ({ focused }) => (
+            <AntDesign
+              name="home"
+              size={24}
+              color={focused ? "#52c46f" : "black"}
+            />
+          ),
+
+          title: "Merhaba Karen!",
+          headerStyle: {
+            backgroundColor: "#b7edbb",
+          },
+          headerTintColor: "#52c460f",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          tabBarLabel: "Ana Sayfa",
+          headerShown: true,
+          headerRight: () => (
+            <Image
+              source={require("../assets/tabBarIcon.png")}
+              style={{
+                width: wp("11%"),
+                height: wp("11%"),
+                marginRight: wp("4%"),
+              }}
+            />
+          ),
         }}
       />
       <Tab.Screen
         name="Scan"
         component={ScanScreen}
         options={{
-          tabBarIcon: () => <AntDesign name="scan1" size={24} color="black" />,
+          tabBarIcon: ({ focused }) => (
+            <AntDesign
+              name="scan1"
+              size={24}
+              color={focused ? "#52c46f" : "black"}
+            />
+          ),
+          tabBarLabel: "Tara",
         }}
       />
       <Tab.Screen
         name="Profile"
         component={Profile}
         options={{
-          tabBarIcon: () => <AntDesign name="user" size={24} color="black" />,
+          tabBarIcon: ({ focused }) => (
+            <AntDesign
+              name="user"
+              size={24}
+              color={focused ? "#52c46f" : "black"}
+            />
+          ),
+
+          tabBarLabel: "Profil",
         }}
       />
     </Tab.Navigator>
--- src/screens/main/HomeScreen.js
+++ src/screens/main/HomeScreen.js
@@ -1,10 +1,121 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput, Animated } from "react-native";
+import React, { useRef, useState } from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default function HomeScreen() {
+  const [input, setInput] = useState("");
+  const progress = useRef(new Animated.Value(0)).current;
+
+  const handleProgressChange = (text) => {
+    setInput(text);
+    const value = parseFloat(text) || 0;
+
+    Animated.timing(progress, {
+      toValue: value,
+      duration: 700, // animasyon süresi (ms cinsinden)
+      useNativeDriver: false, // height animasyonu için false olmalı
+    }).start();
+  };
+
+  const animatedHeight = progress.interpolate({
+    inputRange: [0, 100],
+    outputRange: ["0%", "100%"],
+    extrapolate: "clamp",
+  });
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={{ padding: wp("3%") }} className="flex-1 bg-[#f8f8f8]">
+      <TextInput
+        placeholder="yağ miktarı gir"
+        onChangeText={handleProgressChange}
+        value={input}
+        keyboardType="numeric"
+        style={{ marginBottom: 20 }}
+      />
+      <View
+        style={{
+          padding: wp("2%"),
+        }}
+        className="flex-row justify-around"
+      >
+        <View
+          style={{
+            width: wp("25%"),
+            height: hp("22%"),
+            backgroundColor: "#f7f296",
+            borderRadius: 35,
+            justifyContent: "center",
+            overflow: "hidden",
+            alignItems: "center",
+            elevation: 7,
+          }}
+        >
+          <Animated.View
+            style={{
+              backgroundColor: "#ffda03",
+              width: "100%",
+              height: animatedHeight,
+              position: "absolute",
+              bottom: 0,
+            }}
+          />
+          <Text>Yağ</Text>
+        </View>
+
+        <View
+          style={{
+            width: wp("25%"),
+            height: hp("22%"),
+            backgroundColor: "#ffb49c",
+            borderRadius: 35,
+            overflow: "hidden",
+            justifyContent: "center",
+            alignItems: "center",
+            elevation: 7,
+          }}
+        >
+          <Animated.View
+            style={{
+              backgroundColor: "#ff7a1b",
+              width: "100%",
+              height: animatedHeight,
+              position: "absolute",
+              bottom: 0,
+            }}
+          />
+          <Text>Karbonhidrat</Text>
+        </View>
+
+        <View
+          style={{
+            width: wp("25%"),
+            height: hp("22%"),
+            backgroundColor: "#bcdceb",
+            borderRadius: 35,
+            overflow: "hidden",
+            justifyContent: "center",
+            alignItems: "center",
+            elevation: 7,
+          }}
+        >
+          <Animated.View
+            style={{
+              backgroundColor: "#5e8aed",
+              width: "100%",
+              height: animatedHeight,
+              position: "absolute",
+              bottom: 0,
+            }}
+          />
+          <Text>Protein</Text>
+        </View>
+      </View>
+      <View>
+        <Text>Favoriler</Text>
+      </View>
     </View>
   );
 }
